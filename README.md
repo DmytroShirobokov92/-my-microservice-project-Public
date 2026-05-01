028271d69aff1d4caf516b07b1960dbe0771c5ef
Dmytro Shirobokov <dnc.dmytro.shirobokov@gmail.com>
2026-05-01 11:09:53 +0300

    Add Terraform modules for S3, VPC, and ECR

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# -my-microservice-project-Public
+# DevOps CICD
+
+## Terraform
+
+##### Всі данні та пояснення відносно домашньої роботи №5 у файлі README_terraform.md у гілці lesson-5, папці lesson-5
---
+++ lesson-5/modules/ecr/outputs.tf
@@ -0,0 +1,7 @@
+# lesson-5/modules/ecr/outputs.tf
+
+output "repository_url" {
+    description = "URL of the ECR repository for Docker images"
+    value       = aws_ecr_repository.my_ecr_repo.repository_url
+}  
+
